abc2a17b0e8e55d44f437d43c766886e0074ba16
yhanini <cyberscorp@gmail.com>
2017-03-08 11:34:08 +0100

    add bootstrap css

--- config/module.config.php
+++ config/module.config.php
@@ -1,3 +1,16 @@
 <?php
-return array();
+return array(
+    'templates' => array(
+        'themes' => array(
+            'formation' => array(
+                'label' => 'Formation',
+                'basePath' => realpath(__DIR__ . '/../theme/formation'),
+                'noBootstrap' => true,
+                'css' => array(
+                    '/css/bootstrap.min.css',
+                )
+            ),
+        )
+   )
+);
 
